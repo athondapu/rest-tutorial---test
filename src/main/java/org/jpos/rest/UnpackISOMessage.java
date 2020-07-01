2fd5cd24789e72545fb1b246dfbd4b65f3c9a7c8
stephentwig <step.twig@gmail.com>
2020-07-01 02:36:28 +0100

    removed the ISOMsg isomsg from the try-catch
    initializing the ISOMsg isomsg = null;
    
    works by populating log but gives error:
    Direct self-reference leading to cycle (through reference chain: java.util.HashMap["IsoMsgFromGet"]->org.jpos.iso.ISOMsg["value"])

--- src/main/java/org/jpos/rest/UnpackISOMessage.java
+++ src/main/java/org/jpos/rest/UnpackISOMessage.java
@@ -2,22 +2,60 @@ package org.jpos.rest;
 import org.jpos.iso.ISOException;
 import org.jpos.iso.ISOMsg;
 import org.jpos.iso.packager.GenericPackager;
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.Consumes;
+import javax.ws.rs.PathParam;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+import java.util.HashMap;
+import java.util.Map;
 
 import java.io.InputStream;
 
+
+@Path("/getIsoEle")
 public class UnpackISOMessage {
-    public static void main(String[] args) {
+    @GET
+    @Produces({MediaType.APPLICATION_JSON})
+    @Consumes(MediaType.TEXT_PLAIN)
+    @Path("/{IsoMsgFromGet}")
+
+    //new main
+    public Response echoGet(
+            //@PathParam("name") String name,
+            @PathParam("IsoMsgFromGet") String tMsgFromGet,
+            String tMsgFromBody
+    ) {
+
         UnpackISOMessage iso = new UnpackISOMessage();
+        ISOMsg isoMsg = null;
         try {
-            ISOMsg isoMsg = iso.parseISOMessage();
+             isoMsg = iso.parseISOMessage();
             iso.printISOMessage(isoMsg);
         } catch (Exception e) {
             e.printStackTrace();
         }
+
+
+        Map<String, Object> resp = new HashMap<>();
+        resp.put("success", "true");
+        //resp.put("Name", name);
+        resp.put("IsoMsgFromGet", isoMsg);
+        resp.put("IsoMsgFromBody", tMsgFromBody);
+
+        Response.ResponseBuilder rb = Response.ok(resp,
+                MediaType.APPLICATION_JSON).status(Response.Status.OK);
+        return rb.build();
     }
 
-    private ISOMsg parseISOMessage() throws Exception {
-        String message = "02003220000000808000000010000000001500120604120000000112340001840";
+
+
+
+    public ISOMsg parseISOMessage() throws Exception {
+        String message= "0200FABF469F29E0C12000000000040000221646603898000201580120000000000040000000000005770411101202714425000136931012000411220404110411605190200100C00000000C00000000C00000000C0000000006476117374660389800020158D22042011384200106210910110013693201TERMID01HCB_CODE       HEYZIEL                MAURITIUS      MU480936004151001000000012591313000000516310152001012000001040002682008040000000000VB12ISS     CALT24R15Snk013693013693CALVisaGrp                48000182218Postilion:MetaData270218Postilion:OBS:dCvv111214TRANSACTION_ID111220OriginalPosEntryMode111218Postilion:OBS:dCvv110214TRANSACTION_ID215309101367222006220OriginalPosEntryMode149020";
+
         System.out.printf("Message = %s%n", message);
         try {
             // Load package from resources directory.
@@ -32,7 +70,7 @@ public class UnpackISOMessage {
         }
     }
 
-    private void printISOMessage(ISOMsg isoMsg) {
+    public void printISOMessage(ISOMsg isoMsg) {
         try {
             System.out.printf("MTI = %s%n", isoMsg.getMTI());
             for (int i = 1; i <= isoMsg.getMaxField(); i++) {
